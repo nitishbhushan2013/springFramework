57eab8ae3440f32e330742ae3b32f404f16d4b0d
nitishbhushan2013 <nitishbhushan2013@gmail.com>
2017-09-13 21:59:11 +1000

    spring framework fundamental
    teaching tips - Idea is to walk through with basic fundamental concepts
    for the others team members and students to learn .

--- STS-Spring/springFramework/src/main/java/com/nitish/teach/dao/SpringHibernateDAO.java
+++
@@ -1,9 +0,0 @@
-package com.nitish.teach.dao;
-
-import java.util.List;
-
-import com.nitish.teach.model.Customer;
-
-public interface SpringHibernateDAO {
-	public List<Customer> getAllCustomer();
-}
